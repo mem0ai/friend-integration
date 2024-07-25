329a86e2d6c80ff95e558f243cecd8f42dc7d38e
Deshraj Yadav <deshrajdry@gmail.com>
2024-07-25 14:56:03 -0700

    Update code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from modal import Image, App, Secret, asgi_app, mount
 
-from llm import news_checker
 from models import Memory
 from mem0 import MemoryClient
 
@@ -18,14 +17,7 @@ modal_app = App(
     ],
 )
 
-mem0_api_key = os.getenv("MEM0_API_KEY")
-if not mem0_api_key:
-    raise ValueError(
-        "MEM0_API_KEY is required. You can get it from https://app.mem0.ai"
-    )
-
-
-mem0 = MemoryClient(api_key=mem0_api_key)
+mem0 = MemoryClient(api_key=os.getenv("MEM0_API_KEY", "123"))
 
 
 @modal_app.function(
@@ -44,45 +36,22 @@ def plugins_app():
 # ************ On Memory Created Plugin ************
 # **************************************************
 
-# noinspection PyRedeclaration
-templates = Jinja2Templates(directory="templates")
 
-
-@app.post("/mem0-add")
+@app.post("/mem0")
 def mem0_add(memory: Memory, uid: str):
-    messages = [
-        {
-            "role": "user",
-            "content": f"Here is the transcript of the conversation I had: {memory.transcript}",
-        }
-    ]
+    transcript_segments = memory.transcriptSegments
+    messages = []
+    for segment in transcript_segments:
+        messages.append(
+            {
+                "role": "user" if segment.is_user else "assistant",
+                "content": segment.text,
+            }
+        )
+    if not messages:
+        return {"message": "No messages found"}
+
     mem0.add(messages, user_id=uid)
-    memories = mem0.search(memory.transcript, user_id=uid)
+    memories = mem0.search(messages, user_id=uid)
     response = [row["memory"] for row in memories]
-    return response
-
-
-# *******************************************************
-# ************ On Transcript Received Plugin ************
-# *******************************************************
-
-
-# @app.post("/news-checker")
-# def news_checker_endpoint(uid: str, data: dict):
-#     session_id = data[
-#         "session_id"
-#     ]  # use session id in case your plugin needs the whole conversation context
-#     new_segments = data["segments"]
-#     clean_all_transcripts_except(uid, session_id)
-
-#     transcript: list[dict] = append_segment_to_transcript(uid, session_id, new_segments)
-#     message = news_checker(transcript)
-
-#     if message:
-#         # so that in the next call with already triggered stuff, it doesn't trigger again
-#         remove_transcript(uid, session_id)
-
-#     return {"message": message}
-
-
-# https://e604-107-3-134-29.ngrok-free.app/news-checker
+    return {"message": response}
