00cbef83921f5c62a8fe26a6ae5005e775382e1d
Deshraj Yadav <deshrajdry@gmail.com>
2024-07-25 14:39:45 -0700

    Remove stale code

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 import os
-from typing import List
 
-from fastapi import FastAPI, HTTPException, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from modal import Image, App, Secret, asgi_app, mount
-from multion.client import MultiOn
 
-import templates
-from db import (
-    get_notion_crm_api_key,
-    get_notion_database_id,
-    store_notion_crm_api_key,
-    store_notion_database_id,
-    clean_all_transcripts_except,
-    append_segment_to_transcript,
-    remove_transcript,
-)
 from llm import news_checker
 from models import Memory
-from notion_utils import store_memoy_in_db
 from mem0 import MemoryClient
 
 app = FastAPI()
@@ -81,22 +67,22 @@ def mem0_add(memory: Memory, uid: str):
 # *******************************************************
 
 
-@app.post("/news-checker")
-def news_checker_endpoint(uid: str, data: dict):
-    session_id = data[
-        "session_id"
-    ]  # use session id in case your plugin needs the whole conversation context
-    new_segments = data["segments"]
-    clean_all_transcripts_except(uid, session_id)
+# @app.post("/news-checker")
+# def news_checker_endpoint(uid: str, data: dict):
+#     session_id = data[
+#         "session_id"
+#     ]  # use session id in case your plugin needs the whole conversation context
+#     new_segments = data["segments"]
+#     clean_all_transcripts_except(uid, session_id)
 
-    transcript: list[dict] = append_segment_to_transcript(uid, session_id, new_segments)
-    message = news_checker(transcript)
+#     transcript: list[dict] = append_segment_to_transcript(uid, session_id, new_segments)
+#     message = news_checker(transcript)
 
-    if message:
-        # so that in the next call with already triggered stuff, it doesn't trigger again
-        remove_transcript(uid, session_id)
+#     if message:
+#         # so that in the next call with already triggered stuff, it doesn't trigger again
+#         remove_transcript(uid, session_id)
 
-    return {"message": message}
+#     return {"message": message}
 
 
 # https://e604-107-3-134-29.ngrok-free.app/news-checker
